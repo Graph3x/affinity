2b28e0170d4e0dc72f07dea41d177feb5a1fde55
Filip <hejzlar.filip@seznam.cz>
2023-05-31 10:47:57 +0200

    init2

---
+++ src/homepage.jsx
@@ -0,0 +1,16 @@
+import {React, Component} from 'react';
+
+
+class Homepage extends Component {
+
+  render() {
+    return (
+        <div id='root'>
+            <p>HELL</p>
+        </div>
+    );
+  }
+}
+
+
+export default Homepage;
