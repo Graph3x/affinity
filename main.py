a98854e4d3187da42bd5a3c14c97e84d4048e0bd
Filip <hejzlar.filip@seznam.cz>
2023-06-27 12:51:45 +0200

    api moves

--- main.py
+++ main.py
@@ -4,7 +4,38 @@ import json
 app = FastAPI()
 
 
+# INTER ENDPOINT GLOBALS
+trac_data = {
+        "gps": "NaN",
+        "speed": "NaN",
+        "padak": "0",
+        "magnet": "0"
+    }
+
+Launched = False
+
+
 def struct_module(data: list) -> dict:
+
+    op_code = 1
+
+    if not Launched:
+
+        if trac_data["magnet"] == "1":
+            op_code = 2
+        elif trac_data["magnet"] != "0":
+            op_code = 3
+            global Launched
+            Launched = True
+    else:
+        op_code = 4
+        if trac_data["padak"] != "0":
+            op_code = 5
+    
+        if float(data["speed"]) < 3:
+            op_code = 6
+
+
     return{
         "time": data[0],
         "global_time": data[2],
@@ -17,7 +48,10 @@ def struct_module(data: list) -> dict:
         "temp": data[9],
         "pressure": data[10],
         "humid": data[11],
-        "op_code": 1, #TODO
+        "op_code": 1,
+        "mag": data["magnet"],
+        "gp2": data["gps"],
+        "padak": data["padak"]
     }
 
 
@@ -43,15 +77,15 @@ async def posted_tracker(request: Request):
     sus_data = request.headers['content-type']
 
     sdata = sus_data.split('a')
-
-    data = {
+    global trac_data
+    trac_data = {
         "gps": sdata[0],
         "speed": sdata[1],
         "padak": sdata[2],
         "magnet": sdata[3]
     }
 
-    print(json.dumps(data))
+    print(json.dumps(trac_data))
 
     return "OK"
 
